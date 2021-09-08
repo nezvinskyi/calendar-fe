c612cb8c9f69b71b15fcf94ced6140c28edb3b92
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-09-09 01:50:45 +0200

    adds notifications

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { userOperations } from './redux/user';
+import { sessionSelectors } from './redux/session';
 
 import { PrivatRoute, PublicRoute } from './routes';
 import { CalendarPage, LoginPage, RegistrationPage } from './screens';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   const dispatch = useDispatch();
+  const error = useSelector(sessionSelectors.getError);
+
+  useEffect(() => {
+    toast(error);
+  }, [error]);
+
   useEffect(() => {
     dispatch(userOperations.getCurrentUser());
   }, [dispatch]);
+
   return (
     <>
       <Switch>
@@ -17,6 +29,7 @@ const App = () => {
         <PublicRoute restricted redirectTo="/" path="/login" component={LoginPage} />
         <PrivatRoute path="/" exact component={CalendarPage} redirectTo="/login" />
       </Switch>
+      <ToastContainer />
     </>
   );
 };
