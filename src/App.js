d334ac77b50883e5c84eda067df583bc14707466
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-09-08 23:26:01 +0200

    persist user ingo from localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { userOperations } from './redux/user';
 
 import { PrivatRoute, PublicRoute } from './routes';
 import { CalendarPage, LoginPage, RegistrationPage } from './screens';
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(userOperations.getCurrentUser());
+  }, [dispatch]);
   return (
     <>
       <Switch>
