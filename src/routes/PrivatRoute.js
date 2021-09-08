308b5948b5398edfe52f8d65599ddbdd902d394e
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-09-08 23:28:13 +0200

    gets isAuth from redux store

--- src/routes/PrivatRoute.js
+++ src/routes/PrivatRoute.js
@@ -1,10 +1,9 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-// import { sessionSelectors } from '../redux/session';
+import { sessionSelectors } from '../redux/session';
 
 export default function PrivatRoute({ component: Component, redirectTo, ...routeProps }) {
-  // const isAuth = useSelector(sessionSelectors.getIsAuth);
-  const isAuth = false;
+  const isAuth = useSelector(sessionSelectors.getIsAuth);
   return (
     <Route
       {...routeProps}
--- src/routes/PublicRoute.js
+++ src/routes/PublicRoute.js
@@ -1,9 +1,10 @@
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-// import { sessionSelectors } from '../redux/session';
+import { sessionSelectors } from '../redux/session';
 
 export default function PublicRoute({ component: Component, redirectTo, ...routeProps }) {
-  const isAuth = false;
+  const isAuth = useSelector(sessionSelectors.getIsAuth);
+
   return (
     <Route
       {...routeProps}
