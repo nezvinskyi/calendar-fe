ca1e02b81e906c4a8f585945633fff3b37e71494
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-09-08 23:23:04 +0200

    Create index.js

---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+import CalendarPage from './CalendarPage';
+import LoginPage from './LoginPage';
+import RegistrationPage from './RegistrationPage';
+
+export { CalendarPage, LoginPage, RegistrationPage };
