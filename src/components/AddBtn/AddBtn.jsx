2b0f85be831ef2050d80bab05be67ba5d74dff30
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-09-09 01:26:50 +0200

    adds addbtn and modal

---
+++ src/components/AddBtn/AddBtn.jsx
@@ -0,0 +1,37 @@
+import { Button } from 'react-bootstrap';
+import addIcon from '../../assets/images/add-icon.svg';
+
+import { useDispatch } from 'react-redux';
+import { globalOperations } from '../../redux/global';
+
+const inlineStyles = {
+  addBtn: {
+    backgroundColor: '#0d6efd',
+    width: '44px',
+    height: '44px',
+    borderRadius: '50%',
+    padding: '0px',
+    border: 'none',
+    position: 'fixed',
+    bottom: '2em',
+    right: '2em',
+  },
+};
+
+const AddBtn = () => {
+  const dispatch = useDispatch();
+
+  const openModal = () => {
+    dispatch(globalOperations.openModalAddTransaction());
+  };
+
+  return (
+    <>
+      <Button onClick={openModal} style={inlineStyles.addBtn}>
+        <img src={addIcon} alt="" />
+      </Button>
+    </>
+  );
+};
+
+export default AddBtn;
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import { createPortal } from 'react-dom';
+import { useDispatch } from 'react-redux';
+import { globalOperations } from '../../redux/global';
+import closeIcon from '../../assets/images/close-icon.svg';
+import css from './Modal.module.css';
+
+const modalRootRef = document.getElementById('modal-root');
+
+const Modal = ({ children }) => {
+  const dispatch = useDispatch();
+
+  const onClose = () => {
+    dispatch(globalOperations.closeModalAddTransaction());
+  };
+
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      dispatch(globalOperations.closeModalAddTransaction());
+    }
+  };
+
+  return createPortal(
+    <div className={css.backdrop} onClick={e => handleBackdropClick(e)}>
+      <div className={css.modal}>
+        <div className={css.closeBtnContainer} onClick={onClose}>
+          <img src={closeIcon} alt="" />
+        </div>
+        {children}
+      </div>
+    </div>,
+    modalRootRef,
+  );
+};
+
+export default Modal;
